b648e0f7b2fcf6a005e51712b6d76d39b74f9e0a
Anna-selina Fenzl <afenzl@1-A-29.42heilbronn.de>
2022-06-27 20:23:36 +0200

    with strcmp function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: afenzl <afenzl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:31:16 by afenzl            #+#    #+#             */
-/*   Updated: 2022/05/20 14:32:54 by afenzl           ###   ########.fr       */
+/*   Updated: 2022/06/27 20:22:10 by afenzl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(char *str1, char *str2);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_atoi(const char *str);
